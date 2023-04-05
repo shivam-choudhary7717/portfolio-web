c790b7c7761ae3080abdd6c3d29b40492bb00bc2
shivam-choudhary7717 <shivamchoudhary5602@gmail.com>
2023-04-06 00:20:45 +0530

    Updates

---
+++ precache-manifest.ddc437020d132582d743eac353c446cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "235a67c464fdea05fa61",
+    "url": "/portfolio-web/static/css/main.09cd14f8.chunk.css"
+  },
+  {
+    "revision": "235a67c464fdea05fa61",
+    "url": "/portfolio-web/static/js/main.d64cf8e2.chunk.js"
+  },
+  {
+    "revision": "5aa3f4a28557d65334ba",
+    "url": "/portfolio-web/static/js/runtime~main.f51701c4.js"
+  },
+  {
+    "revision": "0780494ed994b6427dc8",
+    "url": "/portfolio-web/static/css/2.bed910d3.chunk.css"
+  },
+  {
+    "revision": "0780494ed994b6427dc8",
+    "url": "/portfolio-web/static/js/2.7fe603c6.chunk.js"
+  },
+  {
+    "revision": "747ef093c0efba43f1ef",
+    "url": "/portfolio-web/static/js/3.cdf3fc0b.chunk.js"
+  },
+  {
+    "revision": "1e86534f7863908f10b153a2e2dbfe48",
+    "url": "/portfolio-web/static/media/myimg.1e86534f.jpg"
+  },
+  {
+    "revision": "01b19a7e46b38dcfd2d6964f87407d71",
+    "url": "/portfolio-web/static/media/avtaar1.01b19a7e.png"
+  },
+  {
+    "revision": "09e63fe5aa3ebef231ea156c9c9dc6af",
+    "url": "/portfolio-web/static/media/avtaar2.09e63fe5.png"
+  },
+  {
+    "revision": "d0c2f37cc93c694c10ed096f4d463787",
+    "url": "/portfolio-web/static/media/avtaar3.d0c2f37c.png"
+  },
+  {
+    "revision": "e744b88fb47d62f4c1853e04f9f10d01",
+    "url": "/portfolio-web/static/media/wallpaper.e744b88f.jpg"
+  },
+  {
+    "revision": "99dbbea9bbae12c362365c17876731dc",
+    "url": "/portfolio-web/index.html"
+  }
+];
